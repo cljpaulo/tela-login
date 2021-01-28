4a135c2a79110cc84ff6a2af3565d7b868344309
Paulo César Lattaro Jacob <cljpaulo99@gmail.com>
2021-01-28 16:12:38 -0300

    criação dos quadrados que vão ser animação

--- script.js
+++ script.js
@@ -34,3 +34,24 @@ form.addEventListener("animationend", Event => {
         document.querySelector("body").style.overflow = "none";
     }
 });
+
+/* BACKGROUND SQUARES */
+
+const ulSquares = document.querySelector("ul.squares");
+
+for (let i = 0; i < 11; i++) {
+    const li = document.createElement("li");
+
+    const random = (min, max) => Math.random() * (max - min) + min
+
+    const size = Math.floor(random(10, 120));
+
+    const position = random(1, 99);
+
+    li.style.width = `${size}px`;
+    li.style.height = `${size}px`;
+    li.style.bottom = `${size}px`;
+    li.style.left = `${position}%`;
+
+    ulSquares.appendChild(li);
+}
