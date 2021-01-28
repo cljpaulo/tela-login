fd78c21cb572a253d5f315124e42e19cd3671c3f
Paulo César Lattaro Jacob <cljpaulo99@gmail.com>
2021-01-28 15:14:36 -0300

    form sumindo depois de clicar no botão

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ btnLogin.addEventListener("click", Event => {
     Event.preventDefault();
     form.classList.add("form-hide");
 });
+
+form.addEventListener("animationstart", Event => {
+    if (Event.animationName === "down") {
+        document.querySelector("body").style.overflow = "hidden";
+    }
+});
+
+form.addEventListener("animationend", Event => {
+    if(Event.animationName === "down") {
+        form.style.display = "none";
+        document.querySelector("body").style.overflow = "none";
+    }
+});
